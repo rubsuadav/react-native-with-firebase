6dc8fab04f11ab44e6cae7c44e54bb953a76c7b4
rubsuadav <rsuarezdavid@gmail.com>
2023-07-13 21:47:53 +0200

    now we can create users

--- App.jsx
+++ App.jsx
@@ -1,48 +1,11 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./app/auth/Login";
-import List from "./app/auth/List";
-import Details from "./app/auth/Details";
-import { useEffect, useState } from "react";
-import { User, onAuthStateChanged } from "firebase/auth";
-import { FIREBASE_AUTH } from "./firebaseConfig";
+import { Text, View } from "react-native";
+import tw from "twrnc";
+import UserAdminScreen from "./components/UserAdminScreen";
 
-const Stack = createNativeStackNavigator();
-const InsideStack = createNativeStackNavigator();
-
-function InsideLayout() {
-  return (
-    <InsideStack.Navigator>
-      <InsideStack.Screen name="My tools" component={List} />
-      <InsideStack.Screen name="details" component={Details} />
-    </InsideStack.Navigator>
-  );
-}
 export default function App() {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      setUser(user);
-    });
-  }, []);
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
-        {user ? (
-          <Stack.Screen
-            name="Inside"
-            component={InsideLayout}
-            options={{ headerShown: false }}
-          />
-        ) : (
-          <Stack.Screen
-            name="login"
-            component={Login}
-            options={{ headerShown: false }}
-          />
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={tw`p-4 android:pt-2 bg-white dark:bg-black`}>
+      <UserAdminScreen />
+    </View>
   );
 }
