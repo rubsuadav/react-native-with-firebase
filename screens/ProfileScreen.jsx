c2cdc3c7e822ae3d92eddefbb301177aa2fedba8
rubsuadav <rsuarezdavid@gmail.com>
2023-07-23 14:15:41 +0200

    now we can view the corresponding profile's user

--- screens/ProfileScreen.jsx
+++ screens/ProfileScreen.jsx
@@ -1,10 +1,57 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, Button, ImageBackground } from "react-native";
+import tw from "twrnc";
+import { useNavigation } from "@react-navigation/native";
+
+// Importar la imagen de fondo
+import backgroundImage from "../assets/background.jpg";
+import { getUserProfile, handleLogout } from "../utils/Functions";
 
 export default function ProfileScreen() {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    async function fetchData() {
+      await getUserProfile({ setError, setUser });
+    }
+    fetchData();
+  }, []);
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
-    </View>
+    <ImageBackground source={backgroundImage} style={tw`flex-1`}>
+      <View
+        style={tw`flex-1 items-center justify-center bg-opacity-80 bg-gray-900`}
+      >
+        {user && (
+          <>
+            <Text style={tw`text-3xl font-bold text-white mb-8`}>
+              Perfil de {user.displayName}
+            </Text>
+            {error !== "" && <Text style={tw`text-red-500 mb-8`}>{error}</Text>}
+            <TextInput
+              placeholder="Nombre completo"
+              value={user.displayName}
+              style={tw`border border-gray-400 rounded-md w-80 px-4 py-2 mb-4 bg-white`}
+              editable={false}
+            />
+            <TextInput
+              placeholder="Correo electrónico"
+              value={user.email}
+              style={tw`border border-gray-400 rounded-md w-80 px-4 py-2 mb-4 bg-white`}
+              editable={false}
+            />
+            <View style={tw`flex-row justify-between w-80`}>
+              <Button
+                title="Cerrar sesión"
+                onPress={() => handleLogout({ navigation, setError })}
+              />
+            </View>
+          </>
+        )}
+      </View>
+    </ImageBackground>
   );
 }
---
+++ utils/Functions.jsx
@@ -0,0 +1,40 @@
+import { doc, getDoc } from "firebase/firestore";
+import { signOut } from "firebase/auth";
+
+//local imports
+import { FIREBASE_AUTH, FIREBASE_DB } from "../firebaseConfig";
+
+export async function getUserProfile({ setError, setUser }) {
+  const currentUser = FIREBASE_AUTH.currentUser;
+  switch (currentUser) {
+    case null:
+      setError("No se encontró el usuario");
+      break;
+    default:
+      const userRef = doc(FIREBASE_DB, "users", currentUser.uid);
+      const docSnap = await getDoc(userRef);
+      switch (docSnap.exists()) {
+        case true:
+          setUser(docSnap.data());
+          break;
+        case false:
+          setError("No se encontró el usuario");
+          break;
+      }
+      break;
+  }
+}
+export async function handleLogout({ navigation, setError }) {
+  try {
+    await signOut(FIREBASE_AUTH);
+    navigation.navigate("Login");
+  } catch (error) {
+    switch (error.code) {
+      case "auth/user-not-found":
+        setError("El usuario no existe");
+        break;
+      default:
+        break;
+    }
+  }
+}
