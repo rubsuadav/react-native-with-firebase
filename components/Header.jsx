6de51f776cbb8136bdcb3f719038a5cbda8b5983
rubsuadav <rsuarezdavid@gmail.com>
2023-07-31 16:46:49 +0200

    customing the  header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { View, Text, TouchableOpacity, Platform } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Platform, Image } from "react-native";
 import tw from "twrnc";
-import { FIREBASE_AUTH } from "../firebaseConfig";
 import { useNavigation } from "@react-navigation/native";
+import Swal from "sweetalert2";
+
+//local imports
+import { FIREBASE_AUTH } from "../firebaseConfig";
 
 export default function Header({ user }) {
-  const [error, setError] = React.useState("");
+  const [error, setError] = useState("");
   const navigation = useNavigation();
 
   async function handleLogout() {
@@ -14,6 +17,22 @@ export default function Header({ user }) {
       switch (Platform.OS) {
         case "web":
           localStorage.removeItem("token");
+          let timerInterval;
+          await Swal.fire({
+            html: "La sesión se cerrará automáticamente en <b></b> milisegundos.",
+            timer: 1000,
+            timerProgressBar: true,
+            didOpen: () => {
+              Swal.showLoading();
+              const b = Swal.getHtmlContainer().querySelector("b");
+              timerInterval = setInterval(() => {
+                b.textContent = Swal.getTimerLeft();
+              }, 100);
+            },
+            willClose: () => {
+              clearInterval(timerInterval);
+            },
+          });
           window.location.reload();
           break;
         case "android":
@@ -35,21 +54,24 @@ export default function Header({ user }) {
 
   return (
     <View
-      style={tw`flex-row justify-between items-center bg-white h-20 px-4 border-b border-gray-300`}
+      style={tw`flex-row justify-between items-center bg-gray-900 h-20 px-4 border-b border-gray-800`}
     >
-      <Text style={tw`font-bold text-lg`}>Logo de la app</Text>
+      <Image
+        source={require("../assets/logotipo.jpg")}
+        style={{ width: 100, height: 80 }}
+      />
       {error ? <Text style={tw`text-red-500`}>{error}</Text> : null}
       {user ? (
         <TouchableOpacity onPress={handleLogout}>
-          <Text style={tw`ml-4 text-base text-gray-500`}>Cerrar sesión</Text>
+          <Text style={tw`ml-4 text-base text-white`}>Cerrar sesión</Text>
         </TouchableOpacity>
       ) : (
         <View style={tw`flex-row`}>
           <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-            <Text style={tw`ml-4 text-base text-gray-500`}>Iniciar sesión</Text>
+            <Text style={tw`ml-4 text-base text-white`}>Iniciar sesión</Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-            <Text style={tw`ml-4 text-base text-gray-500`}>Registrarse</Text>
+            <Text style={tw`ml-4 text-base text-white`}>Registrarse</Text>
           </TouchableOpacity>
         </View>
       )}
