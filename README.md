6f992f86e618606b3bd75880b1f4ffc2fbcf038a
rubsuadav <rsuarezdavid@gmail.com>
2023-08-05 02:00:14 +0200

    upgrading expo web to expo sdk 49

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 You can follow the documentation here:
 https://docs.expo.dev/guides/using-firebase/
 Deploys to Firebase Hosting:
-'https://prueba-cb62f.web.app/'
+'https://subcripciones-9d96d.web.app/'
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "@firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDXCtpL4a64zIlwAQv-Syj8IBWEYy0LsWo",
-  authDomain: "prueba-cb62f.firebaseapp.com",
+  apiKey: "AIzaSyDvoSqEX5-tDD47b4VoFXkO7iglQOV92G8",
+  authDomain: "subcripciones-9d96d.firebaseapp.com",
   databaseURL:
-    "https://prueba-cb62f-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "prueba-cb62f",
-  storageBucket: "prueba-cb62f.appspot.com",
-  messagingSenderId: "903959212753",
-  appId: "1:903959212753:web:97336278b40b7e94ad4bf0",
+    "https://subcripciones-9d96d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "subcripciones-9d96d",
+  storageBucket: "subcripciones-9d96d.appspot.com",
+  messagingSenderId: "1078641327786",
+  appId: "1:1078641327786:web:f28bf8c49fad5eac419ad3",
 };
 
 // Initialize Firebase
