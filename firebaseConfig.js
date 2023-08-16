1832592da34168d80b2a5f14703075d4c86960ef
rubsuadav <rsuarezdavid@gmail.com>
2023-08-16 17:47:41 +0200

    adding analiytics for configuring storage firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,20 +2,22 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: "subcripciones-9d96d.firebaseapp.com",
-  databaseURL:
-    "https://subcripciones-9d96d-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "subcripciones-9d96d",
-  storageBucket: "subcripciones-9d96d.appspot.com",
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
 export const FIREBASE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
