ac4b997202a8edf6ca1419c202cd9f384f716f87
rubsuadav <rsuarezdavid@gmail.com>
2023-08-16 18:19:39 +0200

    trying to solve storage error in deploy

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,9 +7,10 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_DATABASE_URL,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  authDomain: "subcripciones-9d96d.firebaseapp.com",
+  databaseURL:
+    "https://subcripciones-9d96d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "subcripciones-9d96d",
   storageBucket: "subcripciones-9d96d.appspot.com",
   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_APP_ID,
